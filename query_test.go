b84262ac3c652b0d21bd95c5bd42fb20ce396bd4
Yann Salaün <yann.salaun@ulule.com>
2019-08-07 15:43:43 +0200

    fix example

--- query_test.go
+++ query_test.go
@@ -3,39 +3,37 @@ package query_test
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/yansal/query"
 )
 
-func Example_Validate() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		q, err := query.Validate(r.URL.Query(),
-			query.WithIntParam("goal"),
-			query.WithStringParam("q"),
-			query.WithLangParam("lang"),
-			query.WithCountryParam("country"),
-			query.WithStringsParam("choices", []string{"pending", "processing", "success", "failure"}),
-			query.WithCustomParam("url", func(values []string) (interface{}, error) {
-				if len(values) != 1 {
-					return nil, query.ParamError{Key: "url", Message: fmt.Sprintf("expected one value, got %d", len(values))}
-				}
-				v := values[0]
-				resp, err := http.Get(v)
-				if err != nil {
-					return nil, err
-				}
-				defer resp.Body.Close()
-				return resp.Status, nil
-			}),
-		)
-		if qerr, ok := err.(query.Errors); ok {
-			http.Error(w, qerr.Error(), http.StatusBadRequest)
-			return
-		} else if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprint(w, q.Params)
-	})
-	http.ListenAndServe(":8080", nil)
+func ExampleValidate() {
+	v := url.Values{
+		"unknown": []string{"key"},
+		"goal":    []string{"not an int"},
+		"q":       []string{"two", "strings"},
+		"status":  []string{"foo"},
+		"url":     []string{"http://google.com"},
+	}
+	_, err := query.Validate(v,
+		query.WithIntParam("goal"),
+		query.WithStringParam("q"),
+		query.WithStringsParam("status", []string{"pending", "processing", "success", "failure"}),
+		query.WithCustomParam("url", func(values []string) (interface{}, error) {
+			if len(values) != 1 {
+				return nil, query.ParamError{Key: "url", Message: fmt.Sprintf("expected one value, got %d", len(values))}
+			}
+			v := values[0]
+			resp, err := http.Get(v)
+			if err != nil {
+				return nil, err
+			}
+			defer resp.Body.Close()
+			return resp.Status, nil
+		}),
+	)
+	qerr := err.(query.Errors)
+	fmt.Println(len(qerr))
+	// Output: 4
 }
